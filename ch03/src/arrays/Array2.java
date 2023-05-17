2226e5e48120bcee1b737fecb4804adefde541eb
mynameiseuiseok <sunges1014@naver.com>
2023-05-17 18:35:10 +0900

    third commit

---
+++ ch03/src/arrays/Array2.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// 문자열형 배열 선언 및 사용
+		String[] cars = {"Morning", "Sonata", "Sportage", "K7"};
+		
+		// "Sonata"를 검색하시오
+		System.out.println(cars[1]);
+		
+		// K7을 BMW로 변경하시오
+		cars[3] = "BMW";
+		
+		// 전체 출력
+		for(int i = 0; i < cars.length; i++) {
+			System.out.print(cars[i] + " ");
+		}
+		System.out.println();
+		
+		// 향상된 for 문
+		// for(자료형 변수 : 배열 이름){}
+		for(String car : cars) {
+			System.out.print(car + " ");
+		}
+	}
+}
