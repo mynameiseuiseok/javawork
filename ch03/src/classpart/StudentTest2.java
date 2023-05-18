e7765da0243aa8d759d0a83528e82ca684db87e9
mynameiseuiseok <sunges1014@naver.com>
2023-05-18 15:32:47 +0900

    fourth commit

---
+++ ch03/src/classpart/StudentTest2.java
@@ -0,0 +1,16 @@
+package classpart;
+
+public class StudentTest2 {
+
+	public static void main(String[] args) {
+		// Student 클래스 사용
+		// std1 - 객체, 객체 변수, 인스턴스, 오브젝트
+		Student std1 = new Student(101, "김민재");
+		std1.showInfo();
+		
+		Student std2 = new Student(102, "김영권");
+		std2.showInfo();
+
+	}
+
+}
