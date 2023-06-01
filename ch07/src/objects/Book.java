ab54c2227eecd1657ced38895454fd502986b45a
mynameiseuiseok <sunges1014@naver.com>
2023-06-01 18:21:22 +0900

    6/1 ch07 java commit

---
+++ ch07/src/objects/Book.java
@@ -0,0 +1,37 @@
+package objects;
+
+public class Book {
+
+	// 필드
+	int bookNumber;
+	String BookTitle;
+	
+	// 생성자
+	Book(int bookNumber, String bookTitle){
+		this.bookNumber = bookNumber;
+		this.BookTitle = bookTitle;
+	}
+	
+	// 메서드 재정의
+	@Override
+	public String toString() {
+		return bookNumber + ", " + BookTitle;
+	}
+	
+	// hashCode() 재정의
+	@Override
+	public int hashCode() {
+		return bookNumber;	// 해시코드 반환값과 같은 의미
+	}
+	
+	// equals() 재정의
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Book) { //obj의 Book의 인스턴스(객체)라면
+			Book book = (Book)obj; 	// 강제 형 변환
+			if(this.bookNumber == book.bookNumber)
+				return true; // true 반환
+		}
+		return false;
+	}
+}
---
+++ ch07/src/objects/EqualsTest.java
@@ -0,0 +1,32 @@
+package objects;
+
+public class EqualsTest {
+
+	public static void main(String[] args) {
+
+		String name1 = new String("호날두");
+		String name2 = new String("호날두");
+		
+		// 객체의 메모리 주소 비교
+		System.out.println(name1 == name2); // false
+		// 객체의 값을 비교
+		System.out.println(name1.equals(name2)); // true
+		
+		System.out.println("===================");
+		Book book1 = new Book(1, "혼공 자바");
+		Book book2 = new Book(1, "혼공 자바");
+		
+		System.out.println(book1 == book2);		// false
+		System.out.println(book1.equals(book2));	// false
+		
+		// hashCode() 테스트 - 메모리(인스턴스) 주소 반환(10진수 출력)
+		// name1, name2는 같다.
+		System.out.println(name1.hashCode());
+		System.out.println(name2.hashCode());
+		
+		// book1, book2는 다름 - 재정의하면 같아짐
+		// 동일 객체로 인정하려면 hashcode()와 equals()가 재정의되어야 함
+		System.out.println(book1.hashCode());
+		System.out.println(book2.hashCode());
+	}
+}
